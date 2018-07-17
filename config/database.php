69c24fedb05146bf70a0ccccd9462e1352f7f96a
Pedro Tanaka <pedro.stanaka@gmail.com>
2018-07-17 08:28:45 -0300

    Updating SSL config on database to use env and adding some instructions on readme

--- config/database.php
+++ config/database.php
@@ -64,7 +64,7 @@ return [
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
-            'sslmode' => 'disable',
+            'sslmode' => env('DB_SSL', 'prefer'),
         ],
 
         'sqlsrv' => [
